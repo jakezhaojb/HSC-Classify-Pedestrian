01b075f00f4b3573ec8b3386c35aad5a9ea9f2bc
Junbo Zhao <zhaojunbo1992chasing@gmail.com>
2013-12-09 22:07:52 +0800

    Compiling the mex-file

---
+++ compile.m
@@ -0,0 +1,3 @@
+mex -I./Eigen-3.1.1/ CXXFLAGS='-fPIC -O3 -march=native' features_hsc_1_mex.cc
+mex -I./Eigen-3.1.1/ CXXFLAGS='-fPIC -O3 -march=native' features_hsc_2_mex.cc
+mex -I./Eigen-3.1.1/ CXXFLAGS='-fPIC -O3 -march=native' features_hsc_3_mex.cc
